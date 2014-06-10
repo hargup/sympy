1251dda0d160084aab344e83a5abe81462d63105
Harsh Gupta <gupta.harsh96@gmail.com>
2014-06-10 19:28:02 +0530

    Change the output of solve_univariate to Sympy Set

--- sympy/solvers/tests/test_solve_univariate.py
+++ sympy/solvers/tests/test_solve_univariate.py
@@ -12,6 +12,8 @@ from sympy.solvers.solvers import _invert, unrad, checksol, posify, _ispow, \
 
 from sympy.polys.rootoftools import RootOf
 
+from sympy.sets import FiniteSet
+
 from sympy.utilities.pytest import slow, XFAIL, raises, skip
 from sympy.utilities.randtest import test_numerically as tn
 
@@ -76,47 +78,47 @@ def test_domain_check():
 
 
 def test_polynomial():
-    assert solve_univariate_real(3*x - 2, x) == [Rational(2, 3)]
+    assert solve_univariate_real(3*x - 2, x) == FiniteSet(Rational(2, 3))
 
-    assert set(solve_univariate_real(x**2 - 1, x)) == set([-S(1), S(1)])
-    assert solve_univariate_real(x - y**3, x) == [y ** 3]
+    assert solve_univariate_real(x**2 - 1, x) == FiniteSet(-S(1), S(1))
+    assert solve_univariate_real(x - y**3, x) == FiniteSet(y ** 3)
 
     a11, a12, a21, a22, b1, b2 = symbols('a11,a12,a21,a22,b1,b2')
 
-    assert set(solve_univariate_real(x**3 - 15*x - 4, x)) == set([
+    assert set(solve_univariate_real(x**3 - 15*x - 4, x)) == set(FiniteSet(
         -2 + 3 ** Rational(1, 2),
         S(4),
-        -2 - 3 ** Rational(1, 2)])
+        -2 - 3 ** Rational(1, 2)))
 
 
 def test_solve_rational():
-    assert solve_univariate_real(1/x + 1, x) == [-S.One]
-    assert solve_univariate_real(1/exp(x) - 1, x) == [0]
+    assert solve_univariate_real(1/x + 1, x) == FiniteSet(-S.One)
+    assert solve_univariate_real(1/exp(x) - 1, x) == FiniteSet(0)
 
 
 def test_solve_as_poly_gen_is_pow():
-    assert solve_as_poly(sqrt(1) + 1, x) == []
+    assert solve_as_poly(sqrt(1) + 1, x) == FiniteSet()
 
 
 def test_no_sol_simple():
-    assert solve_univariate_real(4, x) == []
-    assert solve_univariate_real(exp(x), x) == []
+    assert solve_univariate_real(4, x) == FiniteSet()
+    assert solve_univariate_real(exp(x), x) == FiniteSet()
 
 
 def test_no_sol_rational1():
-    assert solve_univariate_real(1/x, x) == []
-    assert solve_univariate_real(-(1 + x)/(2 + x)**2 + 1/(2 + x), x) == []
+    assert solve_univariate_real(1/x, x) == FiniteSet()
+    assert solve_univariate_real(-(1 + x)/(2 + x)**2 + 1/(2 + x), x) == FiniteSet()
 
 
 def test_no_sol_zero():
-    assert solve_univariate_real(-x**2 - 2*x + (x + 1)**2 - 1, x) == []
+    assert solve_univariate_real(-x**2 - 2*x + (x + 1)**2 - 1, x) == FiniteSet()
 
 
 def test_no_sol_rational_extragenous():
     # Simplification is messing up with the solutions of these equations. For example
     # for the one below a root exists for -1 but -1 is not in the domain of the function.
-    assert solve_univariate_real((x/(x + 1) + 3)**(-2), x) == []
-    assert solve_univariate_real((x - 1)/(1 + 1/(x - 1)), x) == []
+    assert solve_univariate_real((x/(x + 1) + 3)**(-2), x) == FiniteSet()
+    assert solve_univariate_real((x - 1)/(1 + 1/(x - 1)), x) == FiniteSet()
 
 
 def test_solve_polynomial_cv_1a():
@@ -124,16 +126,16 @@ def test_solve_polynomial_cv_1a():
     Test for solving on equations that can be converted to
     a polynomial equation using the change of variable y -> x**Rational(p, q)
     """
-    assert solve_as_poly(sqrt(x) - 1, x) == [1]
-    assert solve_as_poly(sqrt(x) - 2, x) == [4]
-    assert solve_as_poly(x**Rational(1, 4) - 2, x) == [16]
-    assert solve_as_poly(x**Rational(1, 3) - 3, x) == [27]
+    assert solve_as_poly(sqrt(x) - 1, x) == FiniteSet(1)
+    assert solve_as_poly(sqrt(x) - 2, x) == FiniteSet(4)
+    assert solve_as_poly(x**Rational(1, 4) - 2, x) == FiniteSet(16)
+    assert solve_as_poly(x**Rational(1, 3) - 3, x) == FiniteSet(27)
 
 
 @XFAIL
 def test_solve_polynomial_multiple_gens():
     assert solve_as_poly(sqrt(x) + x**Rational(1, 3) +
-                         x**Rational(1, 4), x) == [0]
+                         x**Rational(1, 4), x) == FiniteSet(0)
 
 
 @XFAIL
@@ -146,22 +148,22 @@ def test_solve_polynomial_symbolic_param():
     a = Symbol('a', positive=True)
 
     assert set(solve_univariate_real((x**2 - 1)**2 - a, x)) == \
-        set([sqrt(1 + sqrt(a)), -sqrt(1 + sqrt(a)),
-             sqrt(1 - sqrt(a)), -sqrt(1 - sqrt(a))])
+        set(FiniteSet(sqrt(1 + sqrt(a)), -sqrt(1 + sqrt(a)),
+             sqrt(1 - sqrt(a)), -sqrt(1 - sqrt(a))))
 
     assert set(
-        solve_as_poly(4 *x*(1 - a * sqrt(x)), x)) == set([S(0), 1 / a ** 2])
+        solve_as_poly(4 *x*(1 - a * sqrt(x)), x)) == set(FiniteSet(S(0), 1 / a ** 2))
 
 
 def test_solve_polynomial_cv_1b():
     assert set(
-        solve_as_poly(x*(x**(S(1) / 3) - 3), x)) == set([S(0), S(27)])
+        solve_as_poly(x*(x**(S(1) / 3) - 3), x)) == set(FiniteSet(S(0), S(27)))
 
 
 def test_solve_univariate_real_rational():
     """Test solve_univariate_real for rational functions"""
     assert solve_univariate_real((x - y**3) / ((y**2)*sqrt(1 - y**2)), x) \
-        == [y**3]
+        == FiniteSet(y**3)
 
 
 @XFAIL
@@ -169,19 +171,19 @@ def test_issue_7228():
     # In theory this test should have been passed, but the Poly function screws it up.
     # It creates two generators out of it but spliting 4**(a + b) to 4**a*4**b
     assert solve_univariate_real( 4**(2*(x**2) + 2*x) - 8, x) == \
-            [-Rational(3, 2), S.Half]
+            FiniteSet(-Rational(3, 2), S.Half)
 
 
 @XFAIL
 def test_issue_7190():
-    assert solve_univariate_real(log(x - 3) + log(x + 3), x) == [sqrt(10)]
+    assert solve_univariate_real(log(x - 3) + log(x + 3), x) == FiniteSet(sqrt(10))
 
 
 def test_solve_univariate_real_transcendental_1():
-    assert solve_univariate_real(exp(x) - 3, x) == [log(3)]
+    assert solve_univariate_real(exp(x) - 3, x) == FiniteSet(log(3))
     assert set(solve_univariate_real((a*x + b)*(exp(x) - 3), x)) \
-        == set([-b / a, log(3)])
-    assert solve_univariate_real(log(x) - 3, x) == [exp(3)]
+        == set(FiniteSet(-b / a, log(3)))
+    assert solve_univariate_real(log(x) - 3, x) == FiniteSet(exp(3))
 
 
 def test_quintics_1():
@@ -194,74 +196,74 @@ def test_quintics_1():
 
 def test_solve_rational2():
     """Test solve for rational functions"""
-    assert solve_univariate_real(( x - y**3)/((y**2)*sqrt(1 - y**2)), x) == [y**3]
+    assert solve_univariate_real(( x - y**3)/((y**2)*sqrt(1 - y**2)), x) == FiniteSet(y**3)
 
 
 def test_issue_4793_1():
-    assert solve_univariate_real(x*(1 - 5/x), x) == [5]
-    assert solve_univariate_real(-(1 + x)/(2 + x)**2 + 1/(2 + x), x) == []
-    assert solve_univariate_real(-x**2 - 2*x + (x + 1)**2 - 1, x) == []
-    assert solve_univariate_real((x/(x + 1) + 3)**(-2), x) == []
-    assert solve_univariate_real(x/sqrt(x**2 + 1), x) == [0]
+    assert solve_univariate_real(x*(1 - 5/x), x) == FiniteSet(5)
+    assert solve_univariate_real(-(1 + x)/(2 + x)**2 + 1/(2 + x), x) == FiniteSet()
+    assert solve_univariate_real(-x**2 - 2*x + (x + 1)**2 - 1, x) == FiniteSet()
+    assert solve_univariate_real((x/(x + 1) + 3)**(-2), x) == FiniteSet()
+    assert solve_univariate_real(x/sqrt(x**2 + 1), x) == FiniteSet(0)
 
     y = Symbol('y', real=True)
-    assert solve_univariate_real(exp(x) - y, x) == []
+    assert solve_univariate_real(exp(x) - y, x) == FiniteSet()
 
     y = Symbol('y', positive=True)
-    assert solve_univariate_real(exp(x) - y, x) == [log(y)]
+    assert solve_univariate_real(exp(x) - y, x) == FiniteSet(log(y))
 
 
 @XFAIL
 def test_issue_4793_2():
-    assert solve_univariate_real(x + sqrt(x) - 2, x) == [1]
+    assert solve_univariate_real(x + sqrt(x) - 2, x) == FiniteSet(1)
     # I don't know underwhat general techniques this will fit. First I though of rewriting
     # x as sqrt(x)**2 but sqrt(x)**2 is multivalued with values x and -x so the equation
     # is basically this equation is a set of two equations.
 
     assert solve_univariate_real(log(x**2) - y**2/exp(x), x, y, set=True) == \
-        ([y], set([
+        (FiniteSet(y), set(FiniteSet(
             (-sqrt(exp(x)*log(x**2)),),
-            (sqrt(exp(x)*log(x**2)),)]))
-    assert solve_univariate_real(x**2*z**2 - z**2*y**2) == [{x: -y}, {x: y}, {z: 0}]
-    assert solve_univariate_real((x - 1)/(1 + 1/(x - 1))) == []
-    assert solve_univariate_real(x**(y*z) - x, x) == [1]
+            (sqrt(exp(x)*log(x**2)),))))
+    assert solve_univariate_real(x**2*z**2 - z**2*y**2) == FiniteSet({x: -y}, {x: y}, {z: 0})
+    assert solve_univariate_real((x - 1)/(1 + 1/(x - 1))) == FiniteSet()
+    assert solve_univariate_real(x**(y*z) - x, x) == FiniteSet(1)
     raises(NotImplementedError, lambda: solve_univariate_real(log(x) - exp(x), x))
     raises(NotImplementedError, lambda: solve_univariate_real(2**x - exp(x) - 3))
 
 
 def test_atan2():
-    assert solve_univariate_real(atan2(x, 2) - pi/3, x) == [2*sqrt(3)]
+    assert solve_univariate_real(atan2(x, 2) - pi/3, x) == FiniteSet(2*sqrt(3))
 
 
 def test_errorinverses():
-    assert solve_univariate_real(erf(x) - S.One/2, x)==[erfinv(S.One/2)]
-    assert solve_univariate_real(erf(x) - S.One*2, x)==[]
+    assert solve_univariate_real(erf(x) - S.One/2, x)==FiniteSet(erfinv(S.One/2))
+    assert solve_univariate_real(erf(x) - S.One*2, x)==FiniteSet()
 
-    assert solve_univariate_real(erfinv(x) - 2, x)==[erf(2)]
+    assert solve_univariate_real(erfinv(x) - 2, x)==FiniteSet(erf(2))
 
-    assert solve_univariate_real(erfc(x) - S.One, x)==[erfcinv(S.One)]
-    assert solve_univariate_real(erfc(x) - S.One*3, x)==[]
+    assert solve_univariate_real(erfc(x) - S.One, x)==FiniteSet(erfcinv(S.One))
+    assert solve_univariate_real(erfc(x) - S.One*3, x)==FiniteSet()
 
-    assert solve_univariate_real(erfcinv(x) - 2,x)==[erfc(2)]
+    assert solve_univariate_real(erfcinv(x) - 2,x)==FiniteSet(erfc(2))
 
 
 def test_piecewise():
-    assert set(solve_univariate_real(Piecewise((x - 2, Gt(x, 2)), (2 - x, True)) - 3, x)) == set([-1, 5])
+    assert set(solve_univariate_real(Piecewise((x - 2, Gt(x, 2)), (2 - x, True)) - 3, x)) == set(FiniteSet(-1, 5))
 
 
 def test_solve_univariate_complex_polynomial():
     from sympy.abc import x, a, b, c
     assert set(solve_univariate_complex(a*x**2 + b*x + c, x)) == \
-            set([-b/(2*a) - sqrt(-4*a*c + b**2)/(2*a), -b/(2*a) + sqrt(-4*a*c + b**2)/(2*a)])
+            set(FiniteSet(-b/(2*a) - sqrt(-4*a*c + b**2)/(2*a), -b/(2*a) + sqrt(-4*a*c + b**2)/(2*a)))
 
 
 @XFAIL
 def test_solve_univariate_complex_rational():
     from sympy.abc import x, a, b, c
-    assert solve_univariate_complex((x - 1)*(x - I)/(x - 3), x) == [1, I]
+    assert solve_univariate_complex((x - 1)*(x - I)/(x - 3), x) == FiniteSet(1, I)
     # The test is failing because roots return the solutions in a particularly
     # complex form. The answer given by it is
-    # [1/2 + I/2 - sqrt(2)*sqrt(-I)/2, 1/2 + sqrt(2)*sqrt(-I)/2 + I/2]
+    # FiniteSet(1/2 + I/2 - sqrt(2)*sqrt(-I)/2, 1/2 + sqrt(2)*sqrt(-I)/2 + I/2)
     # either I have to simplify the output of the solve
     # for test for mathematical equivalence rather than structural equivalence
 
